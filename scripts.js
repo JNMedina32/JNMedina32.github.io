1eb975311c55d93901a68c654ef92e9c25be8bdf
JNMedina32 <JNMedina32.github.io>
2023-04-03 10:41:25 -0400

    Begin JS code for dropdown menu

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const earlyLife = document.getElementById('#earlyLife');
+//function to change displays of the resume dropdown sections
+const revealResumeSection = (section) => {
+    if(section = earlyLife){
+        earlyLife.style.display = 'inline-block';
+    } 
+}
+
