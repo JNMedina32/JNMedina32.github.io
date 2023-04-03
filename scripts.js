6f0f7778dd0bfa7946776f602c8003ac7c9e0f23
JNMedina32 <JNMedina32.github.io>
2023-04-03 11:24:33 -0400

    Update Resource file

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-const earlyLife = document.getElementById('#earlyLife');
+const earlyLife = document.getElementById('earlyLife');
+const workHistory = document.getElementById('workHistory');
 //function to change displays of the resume dropdown sections
-const revealResumeSection = (section) => {
-    if(section = earlyLife){
+const revealSection = (event) => {
+    if(event){
         earlyLife.style.display = 'inline-block';
+        workHistory.style.display = 'none';
     } 
 }
-
+earlyLife.addEventListener('click', revealSection);
