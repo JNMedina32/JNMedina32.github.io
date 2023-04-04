036b8e85ac4dff2dc09485a75df5db7c558242a6
JNMedina32 <JNMedina32.github.io>
2023-04-04 14:21:37 -0400

    Refractored event listeners for sections

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const goals = document.getElementById('goals');
 const projects = document.getElementById('projects');
 const bodySections = [earlyLife, workHistory, education, interests, goals, projects];
 //Dropdown menu variables
-const dropdownContent = document.getElementById('dropdown')
 const dropdownEarlyLife = document.querySelector('a.earlyLife');
 const dropdownWorkHistory = document.querySelector('a.workHistory');
 const dropdownEducation = document.querySelector('a.education');
@@ -16,62 +15,18 @@ const dropdownGoals = document.querySelector('a.goals');
 const dropdownProjects = document.querySelector('a.projects');
 //function to change displays of the resume when clicked dropdown sections
 const revealAndHide = (section) => {
-    const bodySections = [earlyLife, workHistory, education, interests, goals, projects];
-    for(let key in bodySections){
-        if(section === key){
-           section.style.display = 'inline-block';
+    for(let key of bodySections){
+        if(section.id === key.id){
+           key.style.display = 'inline-block';
         }else{
-           section.style.display = 'none'
+           key.style.display = 'none'
         }
-    }return section
-}
+    }
+};
+//Click events to reveal that section and hide the others
 dropdownEarlyLife.addEventListener('click', () => {revealAndHide(earlyLife)});
-
-// function(e){
-//     earlyLife.style.display = 'block';
-//     workHistory.style.display = 'none'; 
-//     education.style.display = 'none'; 
-//     interests.style.display = 'none'; 
-//     goals.style.display = 'none'; 
-//     projects.style.display = 'none'; 
-// }
-dropdownWorkHistory.addEventListener('click', function(e){
-    earlyLife.style.display = 'none';
-    workHistory.style.display = 'inline-block'; 
-    education.style.display = 'none'; 
-    interests.style.display = 'none'; 
-    goals.style.display = 'none'; 
-    projects.style.display = 'none';
-});
-dropdownEducation.addEventListener('click', function(e){
-    earlyLife.style.display = 'none';
-    workHistory.style.display = 'none'; 
-    education.style.display = 'inline-block'; 
-    interests.style.display = 'none'; 
-    goals.style.display = 'none'; 
-    projects.style.display = 'none';
-});
-dropdownInterests.addEventListener('click', function(e){
-    earlyLife.style.display = 'none';
-    workHistory.style.display = 'none'; 
-    education.style.display = 'none'; 
-    interests.style.display = 'inline-block'; 
-    goals.style.display = 'none';
-    projects.style.display = 'none'; 
-});
-dropdownGoals.addEventListener('click', function(e){
-    earlyLife.style.display = 'none';
-    workHistory.style.display = 'none'; 
-    education.style.display = 'none'; 
-    interests.style.display = 'none'; 
-    goals.style.display = 'inline-block';
-    projects.style.display = 'none'; 
-});
-dropdownProjects.addEventListener('click', function(e){
-    earlyLife.style.display = 'none';
-    workHistory.style.display = 'none'; 
-    education.style.display = 'none'; 
-    interests.style.display = 'none'; 
-    goals.style.display = 'none'; 
-    projects.style.display = 'inline-block';
-});
+dropdownWorkHistory.addEventListener('click', () => {revealAndHide(workHistory)});
+dropdownEducation.addEventListener('click', () => {revealAndHide(education)});
+dropdownInterests.addEventListener('click', () => {revealAndHide(interests)});
+dropdownGoals.addEventListener('click', () => {revealAndHide(goals)});
+dropdownProjects.addEventListener('click', () => {revealAndHide(projects)});
