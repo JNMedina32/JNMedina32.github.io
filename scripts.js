7dd1ebeb716e5605f1f16e32a7d630e7c500ba02
JNMedina32 <JNMedina32.github.io>
2023-04-04 13:35:26 -0400

    Try to refactor click events

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const education = document.getElementById('education');
 const interests = document.getElementById('interests');
 const goals = document.getElementById('goals');
 const projects = document.getElementById('projects');
+const bodySections = [earlyLife, workHistory, education, interests, goals, projects];
 //Dropdown menu variables
 const dropdownContent = document.getElementById('dropdown')
 const dropdownEarlyLife = document.querySelector('a.earlyLife');
@@ -14,14 +15,26 @@ const dropdownInterests = document.querySelector('a.interests');
 const dropdownGoals = document.querySelector('a.goals');
 const dropdownProjects = document.querySelector('a.projects');
 //function to change displays of the resume when clicked dropdown sections
-dropdownEarlyLife.addEventListener('click', function(e){
-    earlyLife.style.display = 'block';
-    workHistory.style.display = 'none'; 
-    education.style.display = 'none'; 
-    interests.style.display = 'none'; 
-    goals.style.display = 'none'; 
-    projects.style.display = 'none'; 
-});
+const revealAndHide = (section) => {
+    const bodySections = [earlyLife, workHistory, education, interests, goals, projects];
+    for(let key in bodySections){
+        if(section === key){
+           section.style.display = 'inline-block';
+        }else{
+           section.style.display = 'none'
+        }
+    }return section
+}
+dropdownEarlyLife.addEventListener('click', () => {revealAndHide(earlyLife)});
+
+// function(e){
+//     earlyLife.style.display = 'block';
+//     workHistory.style.display = 'none'; 
+//     education.style.display = 'none'; 
+//     interests.style.display = 'none'; 
+//     goals.style.display = 'none'; 
+//     projects.style.display = 'none'; 
+// }
 dropdownWorkHistory.addEventListener('click', function(e){
     earlyLife.style.display = 'none';
     workHistory.style.display = 'inline-block'; 
