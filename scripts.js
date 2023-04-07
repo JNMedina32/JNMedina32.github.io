c7b0258f2923d619443998e4ceac31f51a97be8f
JNMedina32 <JNMedina32.github.io>
2023-04-07 12:45:43 -0400

    Add mouse events for nav bar

--- scripts.js
+++ scripts.js
@@ -1,26 +1,8 @@
-//Resume sections variables
-const home = document.getElementById('home');
-const aboutMe = document.getElementById('aboutMe');
-const projects = document.getElementById('projects');
-const contact = document.getElementById('contact');
-const bodySections = [home, aboutMe, projects, interests, goals, contact];
-//Dropdown menu variables
-const dropdownhome = document.querySelector('a.home');
-const dropdownWorkHistory = document.querySelector('a.aboutMe');
-const dropdownEducation = document.querySelector('a.projects');
-const dropdownProjects = document.querySelector('a.contact');
-//function to change displays of the resume when clicked dropdown sections
-const revealAndHide = (section) => {
-    for(let key of bodySections){
-        if(section.id === key.id){
-           key.style.display = 'inline-block';
-        }else{
-           key.style.display = 'none'
-        }
-    }
-};
-//Click events to reveal that section and hide the others
-dropdownhome.addEventListener('click', () => {revealAndHide(home)});
-dropdownWorkHistory.addEventListener('click', () => {revealAndHide(aboutMe)});
-dropdownEducation.addEventListener('click', () => {revealAndHide(projects)});
-dropdownProjects.addEventListener('click', () => {revealAndHide(contact)});
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onmouseenter = () => {
+        document.querySelector('.navbar-img img').src = link.getAttribute('data-src');
+    };
+    link.onmouseleave = () => {
+        document.querySelector('.navbar-img img').src = 'Resources\navbar-images\navbar-img-1';
+    };
+})
